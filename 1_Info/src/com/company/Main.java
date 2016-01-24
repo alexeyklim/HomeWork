5dc033992536d1bdd78c879d4cfeb063825748da
Klimenko Alexey <klimenko.email@gmail.com>
2016-01-24 03:25:17 +0300

    1less

---
+++ 1_Info/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //*********************************************
+        //Личные данные
+        String name = "Клименко Алексей Александрович"; //Ф.И.О.
+        String fam = "женат"; //Семейное положение
+        String work = "БЖД"; //Место работы
+        String post = "инженер-программист";//Должность
+        //**********************************************
+
+        System.out.println("Ф.И.О.:" + name);
+        System.out.println("Семейное положение:" + fam);
+        System.out.println("Место работы:" + work);
+        System.out.println("Должность:" + post);
+
+
+    }
+}
